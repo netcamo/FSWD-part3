44a4876610c2a10d00e6c5c148e2ef4ef9e299d8
netcamo <cyaqublu@gmail.com>
2021-10-31 23:16:55 +0200

    init

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
@@ -86,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
