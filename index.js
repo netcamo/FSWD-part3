b61fe215f94a3154ebeb5756465ab50e25652af7
netcamo <cyaqublu@gmail.com>
2021-11-01 06:40:43 +0200

    Exercises 3.11

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
